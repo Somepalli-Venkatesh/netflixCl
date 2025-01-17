264d7f41b7e61d391314b09cfc2dabb685b79512
Somepalli-Venkatesh <venkateshsomepalli999@gmail.com>
2025-01-17 11:18:30 +0530

    Update PlayCard.jsx

--- my-project/src/components/PlayCard.jsx
+++ my-project/src/components/PlayCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const PlayCard = ({ movie, trailer, showTrailer, setShowTrailer }) => {
+  // Display message when no movie is selected
   if (!movie) {
     return (
       <div className="text-center text-gray-400 mt-10">
@@ -8,6 +9,11 @@ const PlayCard = ({ movie, trailer, showTrailer, setShowTrailer }) => {
       </div>
     );
   }
+
+  // Handle Play button click: Toggle trailer modal
+  const handlePlayClick = () => {
+    setShowTrailer(!showTrailer); // Toggle the visibility of trailer modal
+  };
 
   return (
     <div className="relative bg-gray-800 text-white rounded w-full h-screen">
@@ -19,14 +25,16 @@ const PlayCard = ({ movie, trailer, showTrailer, setShowTrailer }) => {
       <div className="absolute inset-0 flex flex-col justify-between p-6 bg-gradient-to-t from-gray-900/70 via-gray-900/50 to-transparent">
         <h3 className="text-3xl font-bold">{movie.title}</h3>
 
-        {/* Overview with improved design */}
-        <p className="mt-6 text-lg text-opacity-80 leading-relaxed">{movie.overview || "No description available."}</p>
+        {/* Movie Overview */}
+        <p className="mt-6 text-lg text-opacity-80 leading-relaxed">
+          {movie.overview || "No description available."}
+        </p>
 
         <div className="mt-6 flex gap-4 items-center">
-          {/* Play Button */}
+          {/* Play Button: Trigger trailer modal */}
           <button
             className="flex items-center gap-2 px-6 py-2 bg-white text-black text-lg font-bold rounded hover:opacity-90 transition"
-            onClick={() => setShowTrailer(!showTrailer)} // Toggle trailer on click
+            onClick={handlePlayClick}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -36,7 +44,7 @@ const PlayCard = ({ movie, trailer, showTrailer, setShowTrailer }) => {
             >
               <path d="M6.79 11.093l5.256-3.07a.5.5 0 0 0 0-.858L6.79 4.095A.5.5 0 0 0 6 4.498v6.004a.5.5 0 0 0 .79.591z" />
             </svg>
-            {showTrailer ? "Hide Trailer" : "Play"} {/* Change text based on showTrailer state */}
+            {showTrailer ? "Hide Trailer" : "Play"} {/* Toggle text based on trailer state */}
           </button>
 
           {/* More Info Button */}
@@ -54,16 +62,18 @@ const PlayCard = ({ movie, trailer, showTrailer, setShowTrailer }) => {
                 fillRule="evenodd"
                 d="M8 3a.5.5 0 0 1 .5.5v5h4a.5.5 0 0 1 0 1h-4v4a.5.5 0 0 1-1 0v-4H3.5a.5.5 0 0 1 0-1H7.5v-5A.5.5 0 0 1 8 3z"
               />
-            </svg>My List
+            </svg>
+            My List
           </button>
         </div>
 
+        {/* Trailer Modal: Show trailer when `showTrailer` is true */}
         {showTrailer && trailer && (
           <div className="mt-4">
             <iframe
               width="100%"
               height="315"
-              src={trailer}
+              src={trailer} // No autoplay query here
               frameBorder="0"
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
